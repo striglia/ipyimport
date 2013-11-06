96e9c7b099a1d47701b899b20aff92647a898846
Scott Triglia <scott.triglia@gmail.com>
2013-11-05 22:22:17 -0800

    Initial tox file
    Also adds gitignore and setup.py for use by tox.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ipyimport',
+    version='0.1.0',
+    description='Automatic identification of python imports',
+    author='Scott Triglia',
+    author_email='scott.triglia@gmail.com',
+    url='http://www.github.com/striglia/ipyimport',
+    packages=['ipyimport'],
+)
